7fdc2439d360881af3516bf22ffb6b148b6db9ef
Hitchcock <cemekaoji@gmail.com>
2023-04-05 00:38:23 +0100

    completed the calculator challenge

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,30 @@
+// theme toggle function
+let toggleSwitch = document.getElementsByClassName('Button')[0]
+var root = document.querySelector(':root');
+
+
+// theme selector function 
+function go_to_1() {
+    toggleSwitch.classList.add('horizTranslate1');
+    toggleSwitch.classList.remove('horizTranslate2');
+    toggleSwitch.classList.remove('horizTranslate3');
+    root.className = ("themeone");
+}
+
+function go_to_2() {
+    toggleSwitch.classList.add('horizTranslate2');
+    toggleSwitch.classList.remove('horizTranslate3');
+    toggleSwitch.classList.remove('horizTranslate1');
+    root.className = ("themeTwo");
+}
+
+function go_to_3() {
+    toggleSwitch.classList.add('horizTranslate3');
+    toggleSwitch.classList.remove('horizTranslate2');
+    toggleSwitch.classList.remove('horizTranslate1');
+    root.className = ("themeThree");
+}
+// calculator evaluation caption
 function val(result){
     form.disp.value = form.disp.value + result;
 }
